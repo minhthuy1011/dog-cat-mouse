35a63830acdd7e81f6b68ea79fa49aaf0f877fdd
minhthuy <dothi.bethuytt@gmail.com>
2023-02-23 17:50:36 +0700

    Horse and mouse done

--- Horse.js
+++ Horse.js
@@ -4,6 +4,6 @@ Horse.prototype.run = function(){
     console.log("Running");
 
 Horse.prototype.jump = function(){
-    console.log("Jumpning");
+    console.log("Jumping");
 
 module.exports = Horse
